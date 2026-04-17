4c9334f5c659319b525be6930afc364f779905d2
C-Fil9 <dawndz12345@gmail.com>
2026-04-17 20:25:39 +0700

    add vercel.json

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,6 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(`🚀 Server running at http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
